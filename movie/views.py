14cd3426dcd53612410f1b9fbfe6812b4ae4c9f6
Danielzooo <104256692+Danielzooo@users.noreply.github.com>
2022-09-08 18:54:13 +0600

    video 7

--- movie/views.py
+++ movie/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.base import View
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView
 from .models import Movie
-
+from .forms import ReviewForm
 
 class MoviesView(ListView):
     model = Movie
@@ -14,4 +14,17 @@ class MovieDetailView(View):
     def get(self, request, slug):
         movies = Movie.objects.get(url=slug)
         return render(request, 'html/moviesingle.html', {'movie': movies})
+
+
+class AddReviews(View):
+    def post(self, request, pk):
+        form = ReviewForm(request.POST)
+        movie = Movie.objects.get(id=pk)
+        if form.is_valid():
+            form = form.save(commit=False)
+            if request.POST.get('parent', None):
+                form.parent_id = int(request.POST.get('parent'))
+            form.movie = movie
+            form.save()
+        return redirect(movie.get_absolute_url())
 
